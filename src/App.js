682cf2f2d4cc9ee6c6cebc39ff1d99be21cc5f3e
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-04-20 09:19:23 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Cart from './cart';
+import CheckOut from './checkOut';
+import OrderComplete from './order-complete';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <nav>
+            <ul>
+              <li className="nav"><a href='/'>Cart</a></li>
+              <li className="nav"><a href='/checkOut'>Check Out</a></li>
+              <li className="nav"><a href='order-complete'>Order Complete</a></li>
+            </ul>
+              </nav>
+              <BrowserRouter>
+                    <Route exact path={'/'} component={Cart}/>
+                    <Route exact path={'/checkOut'} component={CheckOut}/>
+                    <Route exact path={'/order-complete'} component={OrderComplete}/>
+              </BrowserRouter>
       </div>
     );
   }
---
+++ src/order-complete.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class OrderComplete extends Component {
+    render() {
+        return (
+            <p>order complete</p>
+            
+        );
+    }
+}
